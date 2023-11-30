dd22885b3a24976058c0b5ae0b07727580117a4b
Gabriel Johnson <147633803+ScaryGhost77467@users.noreply.github.com>
2023-11-30 10:55:24 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const quotes = [
 	"Your time is limited, don't waste it living someone else's life. - Steve Jobs",
 	"Strive not to be a success, but rather to be of value. - Albert Einstein",
 	"The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+	"That's one small step for man, one giant leap for mankind. - Neil Armstrong",
+	"The only thing we have to fear is fear itself. - Franklin D. Roosevelt",
+	"In the beginning, God created the heavens and the earth. - The Bible, Genesis 1:1",
+	"The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
 	];
 
 function generateQuote() {
